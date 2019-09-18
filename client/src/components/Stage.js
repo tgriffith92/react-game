8295670effdc17eb7900bdb704aaedd1e027bcb7
terencegriffithCB5 <terence.griffith92@gmail.com>
2019-09-18 09:57:23 -0400

    add the canvas to the stage component

--- client/src/components/Stage.js
+++ client/src/components/Stage.js
@@ -6,20 +6,21 @@ const canvasStyle = {
     height: 800
 }
 
-//create a 2d canvas
-const canvasContext = canvas.getContext('2d');
+
 
 
 class Stage extends Component {
 
-    canvasColor() {
-        canvasContext.fillRect(0,0,100,100)
+    componentDidMount() {
+        //create a 2d canvas
+        const ctx = canvas.getContext('2d');
+        ctx.fillRect(0,0,100,100);
     }
 
     render() {
         return (
             //Give canvas the specified style
-            <canvas style={canvasStyle}></canvas>
+            <canvas ref="canvas" style={canvasStyle}></canvas>
         )
     }
 
