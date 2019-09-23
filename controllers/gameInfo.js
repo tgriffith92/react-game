bd32db439de1d7b1ab28b565ea26f6bca63ba687
terencegriffithCB5 <terence.griffith92@gmail.com>
2019-09-23 17:36:56 -0400

    add the model API for the game data

---
+++ controllers/gameInfo.js
@@ -0,0 +1,49 @@
+/* Step 1 import express
+ *
+ */
+const express = require('express')
+
+/* Step 2
+ *
+ * Import the api files from the models
+ *
+ * TODO: change the file path to the models file you'll need to use.
+ * TODO: rename this from `templateApi` to something more sensible (e.g:
+ * `shopsAPI`)
+ *
+ * NOTE: You may need to import more than one API to create the 
+ * controller you need.
+ * 
+ */
+const gameApi = require('../models/gameInfo.js')
+
+/* Step 3 
+ * 
+ * Create a new router.
+ *
+ * the router will "contain" all the request handlers that you define in this file.
+ * TODO: rename this from templateRouter to something that makes sense. (e.g:
+ * `shopRouter`)
+ */
+const gameRouter = express.Router()
+
+/* Step 4
+ * 
+ * TODO: Put all request handlers here
+ */
+
+gameRouter.get('/:gameId', (req, res) => {
+    gameApi.getGameById(req.params.gameId)
+        .then((singleGame) => {
+            res.send(singleGame)
+        })
+})
+
+/* Step 5
+ *
+ * Export the router from the file.
+ *
+ */
+module.exports = {
+    gameRouter
+}
---
+++ models/gameInfo.js
@@ -0,0 +1,52 @@
+/* 
+ * Place all functions, classes, and/or DB schemas here for a single 
+ * model.
+ */
+
+/* Step 1
+ *
+ * TODO: import mongoose connection
+ * NOTE: skip this if you are not using mongoose
+ *
+ */
+const mongoose = require('./connection.js')
+
+/* Step 2
+ *
+ * TODO: create model schema 
+ * NOTE: skip this if you are not using mongoose
+ *
+ */
+const GameSchema = new mongoose.Schema({
+  color: String,
+  level: Number
+})
+
+/* Step 3
+ *
+ * TODO: create collection API
+ * NOTE: skip this if you are not using mongoose
+ *
+ */
+const GameCollection = mongoose.model('Game', GameSchema)
+
+/* Step 4
+ *
+ *
+ */
+
+
+const getGameById = (gameId) => {
+
+  return GameCollection.findById(gameId)
+}
+
+/* Step 5
+ *
+ * TODO: export all functions from this file by adding their names as keys to this
+ * object
+ */
+module.exports = {
+  getGame,
+  getGameById
+}
